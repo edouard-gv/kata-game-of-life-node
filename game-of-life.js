edbae98ee5c94e6949e51df67f4bfea5a070bc5d
Édouard Gomez-Vaëz <edouard@gomez-vaez.com>
2018-12-08 13:23:29 +0100

    [VERT] contains général

--- game-of-life.js
+++ game-of-life.js
@@ -13,7 +13,7 @@ function livingNeighboursCount(map, cell) {
 }
 
 function contains(map, cell) {
-    return false;
+    return map.some(c => c[0]===cell[0] && c[1]===cell[1]);
 }
 
 module.exports = {next, livingNeighboursCount, contains};
