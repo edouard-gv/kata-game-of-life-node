3785c56940ae10df058bbeb11c4047c346e05463
Édouard Gomez-Vaëz <edouard@gomez-vaez.com>
2018-12-08 13:08:02 +0100

    [ROUGE] nombre de voisins vivants dans une carte vide

--- game-of-life.spec.js
+++ game-of-life.spec.js
@@ -1,6 +1,19 @@
 test = require("tape");
 game = require("./game-of-life");
 
+// Les configurations remarquables
+/**
+ * ....
+ * .**.
+ * .**.
+ * ....
+ * Configuration idempotente: next(stable) = stable
+ * Vérifie que toute cellule entourée de 3 cellules survie.
+ */
+
+const stable = [[1,1],[2,1],[1,2],[2,2]];
+
+
 test("une carte vide donne une carte vide", function(t) {
     t.deepEqual(game.next([]), []);
     t.end();
@@ -8,5 +21,15 @@ test("une carte vide donne une carte vide", function(t) {
 
 test("un solitaire donne une carte vide", function(t) {
     t.deepEqual(game.next([[1,1]]), []);
+    t.end();
+});
+
+test.skip("configuration stable", function(t) {
+   t.deepEqual(game.next(stable), stable);
+   t.end();
+});
+
+test("nombre de voisins vivants vide", function(t) {
+    t.equals(game.livingNeighboursCount([], [1,1]), 0);
     t.end();
 });
