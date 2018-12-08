a17cfb637645a2940e4309fb43daa98d06a34ca4
Édouard Gomez-Vaëz <edouard@gomez-vaez.com>
2018-12-08 12:18:57 +0100

    [VERT] solitaire

--- game-of-life.spec.js
+++ game-of-life.spec.js
@@ -5,3 +5,8 @@ test("une carte vide donne une carte vide", function(t) {
     t.deepEqual(game.next([]), []);
     t.end();
 });
+
+test("un solitaire donne une carte vide", function(t) {
+    t.deepEqual(game.next([[1,1]]), []);
+    t.end();
+});
