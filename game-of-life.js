dd878defe5b5de4371ef29f7fc39b4ed7236ca16
Édouard Gomez-Vaëz <edouard@gomez-vaez.com>
2018-12-08 13:28:05 +0100

    [VERT] voisins

--- game-of-life.js
+++ game-of-life.js
@@ -12,8 +12,17 @@ function livingNeighboursCount(map, cell) {
     return 0;
 }
 
+function neighbours(cell) {
+    const x = cell[0];
+    const y = cell[1];
+    return [[x-1, y-1], [x, y-1], [x+1, y-1],
+            [x, y-1], [x, y+1],
+            [x+1,y-1],[x+1,y],[x+1,y+1]];
+}
+
+
 function contains(map, cell) {
     return map.some(c => c[0]===cell[0] && c[1]===cell[1]);
 }
 
-module.exports = {next, livingNeighboursCount, contains};
+module.exports = {next, livingNeighboursCount, contains, neighbours};
