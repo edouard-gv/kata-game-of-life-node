accbf17c29a7684077c918ba2dfd160ad9d1d250
Édouard Gomez-Vaëz <edouard@gomez-vaez.com>
2018-12-24 12:56:57 +0100

    [VERT] version neighbours triviale OK.

--- game-of-life.js
+++ game-of-life.js
@@ -1,7 +1,11 @@
-var game = Function;
+var game = {
+    next: function(previous) {
+        return previous;
+    },
 
-game.next = function(previous) {
-    return previous;
-};
+    neighbours: function(cell) {
+        return([[-1,-1], [0,-1], [1,-1], [-1,0], [1,0], [-1,1], [0,1], [1,1]]);
+    }
+}
 
 module.exports = game;
