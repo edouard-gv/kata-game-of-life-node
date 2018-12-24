64d728f59fd56c6d7c3534bfad8de491b596b366
Édouard Gomez-Vaëz <edouard@gomez-vaez.com>
2018-12-24 12:43:31 +0100

    [ROUGE] test mort d'une cellule unique

--- game-of-life.spec.js
+++ game-of-life.spec.js
@@ -1,7 +1,12 @@
 const game = require('./game-of-life');
 const test = require('tape');
 
-test('final-one', function(t) {
+test('empty map gives empty map', function(t) {
     t.deepEqual(game.next([]), []);
+    t.end();
+});
+
+test('one cell should die', function(t) {
+    t.deepEqual(game.next([[1,1]]), []);
     t.end();
 });
