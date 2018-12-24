bee0fd8cab7a9b022905905cc5225d3dbfea1029
Édouard Gomez-Vaëz <edouard@gomez-vaez.com>
2018-12-24 15:04:16 +0100

    [REFACTO] Argh besoin de changer ma déclaration de module pour avoir accès au méthodes en interne. Je commente les tests en cours et refactorise le code dans l'état précédent

--- game-of-life.js
+++ game-of-life.js
@@ -1,22 +1,27 @@
-var game = {
-    next: function(previous) {
-        return previous;
-    },
+const next = function(previous) {
+    return previous;
+};
 
-    neighbours: function(cell) {
-        var x=cell[0];
-        var y=cell[1];
+const neighbours = function(cell) {
+    var x=cell[0];
+    var y=cell[1];
 
-        return([[x-1,y-1], [x,y-1], [x+1,y-1], [x-1,y], [x+1,y], [x-1,y+1], [x,y+1], [x+1,y+1]]);
-    },
+    return([[x-1,y-1], [x,y-1], [x+1,y-1], [x-1,y], [x+1,y], [x-1,y+1], [x,y+1], [x+1,y+1]]);
+};
 
-    contains: function(map, cell) {
-        return map.some(c => c[0] === cell[0] && c[1] === cell[1]);
-    },
+const contains = function(map, cell) {
+    return map.some(c => c[0] === cell[0] && c[1] === cell[1]);
+};
 
-    livingNeighbours(map, cell) {
-        return [];
-    }
-}
+const livingNeighbours = function(map, cell) {
+    const cellNeighbours = [];
+/*        for (const c of neighbours(cell)) {
+        if (contains(map, c)) {
+            cellNeighbours.push(c);
+        }
+    } */
 
-module.exports = game;
+    return cellNeighbours;
+};
+
+module.exports = {next, neighbours, livingNeighbours, contains}
--- game-of-life.spec.js
+++ game-of-life.spec.js
@@ -26,12 +26,12 @@ test('living-neighbours', function(t) {
     t.end();
 });
 
-test('living-neighbours-two', function(t) {
+test.skip('living-neighbours-two', function(t) {
     t.true(game.contains(game.livingNeighbours([[0,1], [1,0]], [1,1]), [0,1]));
     t.end();
 });
 
-test('living-neighbours-two', function(t) {
+test.skip('living-neighbours-two', function(t) {
     t.true(game.contains(game.livingNeighbours([[0,1], [1,0], [1,3]], [1,1]), [1,0]));
     t.end();
 });
